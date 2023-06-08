21bbaba842f98fbef03f84d6a72290e6380e7625
Simon hovmand <simonhovmand@gmail.com>
2023-06-08 16:05:09 +0200

    Automaten

--- c#/Automaten/Automaten/Item.cs
+++ c#/Automaten/Automaten/Item.cs
@@ -5,7 +5,7 @@
         public int id; //Making the int id viable
         public int price; //Making the int price viable
         public string name; //Making the string name viable
-        public int amount { get; set; } //Making the int amount vibale, with a get; set; to regulate the amount when a item is bought
+        public int amount { get; set; } //Makinzg the int amount vibale, with a get; set; to regulate the amount when a item is bought
         public string flavor; //Making the string flavor viable
         public string size; //Making the string size viable
 
--- c#/Automaten/Automaten/ItemService.cs
+++ c#/Automaten/Automaten/ItemService.cs
@@ -1,4 +1,6 @@
-﻿namespace Automaten
+﻿using Npgsql;
+
+namespace Automaten
 {
     internal class ItemService
     {
@@ -13,6 +15,29 @@
             items.Add(new Drink(5, 18, "FaxeKondi", 16, "Normal", "0.5L"));
             items.Add(new Drink(6, 18, "FaxeKondi", 23, "Light", "0.5L"));
             items.Add(new Drink(7,18, "FaxeKondi", 35, "Ananas", "0.5L"));
+
+            NpgsqlConnection conn = new NpgsqlConnection("Host=127.0.0.1;Port=5432;Database=VendingMachine;Username=postgres;Password=2312");
+            conn.Open();
+
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * from item", conn);
+
+            NpgsqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine($"{rdr[0]}");
+            }
+            conn.Close();
+            Program pgr = new Program();
+
+            //***ExecuteNonQuery****
+            //pgr.insertData();
+            // pgr.updateData();
+            //pgr.deleteData();
+
+            //***param****
+            //pgr.param();
+
         }
 
         public void RemoveAmount(int choise) //Method to remove a item (the amount of items) when 1 is bought
